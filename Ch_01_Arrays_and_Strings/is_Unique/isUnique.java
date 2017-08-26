867b0873df4db820a536831911b58332abaf01e9
Mourya <mourya@Mouryas-MacBook-Pro.local>
2017-08-26 13:10:05 -0400

    completed isUnique question. Added gitignore

---
+++ Ch_01_Arrays_and_Strings/is_Unique/isUnique.java
@@ -0,0 +1,25 @@
+
+public class isUnique {
+
+	public static boolean isUniqueChars(String str) {
+    if(str.length()>128){
+      return false;
+    }
+    boolean[] uniqueList = new boolean[128];
+    for (int i=0; i<str.length(); i++) {
+      int val = str.charAt(i);
+      if (uniqueList[val]) {
+        return false;
+      }
+      uniqueList[val] = true;
+    }
+    return true;
+	}
+
+	public static void main(String[] args) {
+		String[] words = {"abcde", "hello", "apple", "kite", "padle"};
+		for (String word : words) {
+			System.out.println(word + ": " + isUniqueChars(word));
+		}
+	}
+}
