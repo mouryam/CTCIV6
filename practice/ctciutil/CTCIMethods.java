a012799f2fda96ecdef8c89c2a81d42c2c8f43c1
Mourya Meda <mouryameda@Mouryas-MacBook-Pro.local>
2018-01-24 08:38:08 -0500

    Added CoinDP problem

--- practice/ctciutil/CTCIMethods.java
+++ practice/ctciutil/CTCIMethods.java
@@ -106,7 +106,7 @@ public class CTCIMethods {
 	public static String toFullBinaryString(int a) {
 		String s = "";
 		for (int i = 0; i < 32; i++) {
-			Integer lsb = new Integer(a & 1);
+			Integer lsb = Integer.valueOf(a & 1);
 			s = lsb.toString() + s;
 			a = a >> 1;
 		}
--- practice/extra_practice/CoinDP.java
+++ practice/extra_practice/CoinDP.java
@@ -3,19 +3,32 @@ package practice.extra_practice;
 import java.util.*;
 
 public class CoinDP{
-
   public static void main(String[] args){
-    int change = 79;
-    int[] denominations = {25, 10, 5, 1};
-
-    System.out.println( makeChange(change, denominations));
-
+    long change = Long.parseLong(args[0]);
+    long[] denominations = {25, 10, 5, 1};
+    System.out.println(makeChange(change, 0, denominations, new HashMap<String,Integer>()));
   }
 
+  public static int makeChange(long change, int index, long[] denominations, HashMap<String,Integer> memo){
+  	if (change==0) {return 1;}
+  	if (index >= denominations.length){return 0;}
+  	String key = change+"-"+index;
+  	if(memo.containsKey(key)){
+  		return memo.get(key);
+  	}
+  	long amountWithCoin = 0;
+  	int ways = 0;
+  	while(amountWithCoin<=change){
+  		long remaining = change-amountWithCoin;
+  		ways+=makeChange(remaining, index+1, denominations, memo);
+  		amountWithCoin+=denominations[index];
+  	}
+  	memo.put(key, ways);
+  	return ways;
+  }
 
+  public static int getMinCoinNumber(int total, long[] denominations){
 
-
-
-
+  }
 
 }
