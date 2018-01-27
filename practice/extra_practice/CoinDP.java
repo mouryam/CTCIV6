0b84450901c05f4ba0182399bdbd59b2f3ce4586
Mourya Meda <medamourya@gmail.com>
2018-01-27 15:42:17 -0500

    Finished out CoinDP problem, added minumum number of ways to get change

--- practice/extra_practice/CoinDP.java
+++ practice/extra_practice/CoinDP.java
@@ -4,9 +4,19 @@ import java.util.*;
 
 public class CoinDP{
   public static void main(String[] args){
-    long change = Long.parseLong(args[0]);
+    long change;
+    try{
+    change = Long.parseLong(args[0]);
+    }catch(NumberFormatException e){
+      throw new NumberFormatException("Please input a type long!");
+    }catch(ArrayIndexOutOfBoundsException ae){
+      throw new ArrayIndexOutOfBoundsException("Please input a total number!");
+    }
     long[] denominations = {25, 10, 5, 1};
-    System.out.println(makeChange(change, 0, denominations, new HashMap<String,Integer>()));
+    System.out.println("Number of ways: "+makeChange(change, 0, denominations, new HashMap<String,Integer>()));
+
+    HashMap<Long, List<Long>> minMap = getMinCoinNumber(change, denominations);
+    System.out.println("Minimum number of coins needed: "+ minMap);
   }
 
   public static int makeChange(long change, int index, long[] denominations, HashMap<String,Integer> memo){
@@ -27,8 +37,50 @@ public class CoinDP{
   	return ways;
   }
 
-  public static int getMinCoinNumber(int total, long[] denominations){
+  public static HashMap<Long, List<Long>> getMinCoinNumber(long total, long[] denominations){
+    /*
+        0  1  2  3  4  5  6  7  8  9  10  
+    1   ~  1  2  3  4  5  6  7  8  9  10 
+    5   ~  1  2  3  4  1  2  3  4  5  2
+    25  ..... 
+    */
+    HashMap<Long, List<Long>> outputMap = new HashMap<Long,List<Long>>();
+
+    long[] T = new long[(int)total+1];
+    long[] R = new long[(int)total+1];
+
+    Arrays.fill(T,Integer.MAX_VALUE-1);
+    T[0]= 0;
+    Arrays.fill(R, -1);
+
+    for (int i=0; i< denominations.length; i++){
+      for(int j=1; j<=total; j++){
+        if( j>= denominations[i]){
+          if(T[j] > (T[j-(int)denominations[i]]+1)){
+            T[j] = T[j-(int)denominations[i]]+1;
+            R[j] = i;
+          }
+        }
+      }
+    }
+    List<Long> coinList = new ArrayList<Long>();
+    long amount = total;
+    while(amount>0){
+      coinList.add(denominations[(int)R[(int)amount]]);
+      amount -= denominations[(int)R[(int)amount]];
+    }
+    outputMap.put(T[(int)total], coinList);
+    return outputMap;
 
   }
+
+
+
+
+
+
+
+
+
 
 }
