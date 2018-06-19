937104a388b98f91a87631cf6b5280e22488c509
Mourya Meda <medamourya@gmail.com>
2018-06-18 23:21:22 -0400

    Added TicTacToe problem

---
+++ practice/aPractice/TicTac.java
@@ -0,0 +1,117 @@
+/**
+Design a Tic-tac-toe game that is played between two players on a n x n grid.
+
+You may assume the following rules:
+
+A move is guaranteed to be valid and is placed on an empty block.
+Once a winning condition is reached, no more moves is allowed.
+A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+Example:
+Given n = 3, assume that player 1 is "X" and player 2 is "O" in the board.
+
+TicTacToe toe = new TicTacToe(3);
+
+toe.move(0, 0, 1); -> Returns 0 (no one wins)
+|X| | |
+| | | |    // Player 1 makes a move at (0, 0).
+| | | |
+
+toe.move(0, 2, 2); -> Returns 0 (no one wins)
+|X| |O|
+| | | |    // Player 2 makes a move at (0, 2).
+| | | |
+
+toe.move(2, 2, 1); -> Returns 0 (no one wins)
+|X| |O|
+| | | |    // Player 1 makes a move at (2, 2).
+| | |X|
+
+toe.move(1, 1, 2); -> Returns 0 (no one wins)
+|X| |O|
+| |O| |    // Player 2 makes a move at (1, 1).
+| | |X|
+
+toe.move(2, 0, 1); -> Returns 0 (no one wins)
+|X| |O|
+| |O| |    // Player 1 makes a move at (2, 0).
+|X| |X|
+
+toe.move(1, 0, 2); -> Returns 0 (no one wins)
+|X| |O|
+|O|O| |    // Player 2 makes a move at (1, 0).
+|X| |X|
+
+toe.move(2, 1, 1); -> Returns 1 (player 1 wins)
+|X| |O|
+|O|O| |    // Player 1 makes a move at (2, 1).
+|X|X|X|
+
+Could you do better than O(n2) per move() operation?
+*/
+
+package practice.aPractice;
+import java.util.*;
+
+public class TicTac{
+	public static void main(String[] args) {
+		TicTacToe toe = new TicTacToe(3);
+		toe.move(0, 0, 1);
+		toe.move(0, 2, 2);
+		toe.move(2, 2, 1);
+		toe.move(1, 1, 2);
+		toe.move(2, 0, 1);
+		toe.move(1, 0, 2);
+		toe.move(2, 1, 1);
+
+	}
+
+
+
+	static class TicTacToe{
+		private int[] rows;
+		private int[] cols;
+		private int diag;
+		private int xdiag;
+		private int n;
+
+		public TicTacToe(int n){
+			this.n = n;
+			rows = new int[n];
+			cols = new int[n];
+			diag = 0;
+			xdiag = 0;
+		}
+
+		public int move(int row, int col, int player){
+			int count = player == 1? 1 : -1;
+			int result = 0;
+
+			rows[row] += count;
+			cols[col] += count;
+
+			if(row==col){
+				diag += count;
+			}
+			if(row+col == n-1){
+				xdiag += count;
+			}
+
+			if(Math.abs(rows[row]) == n ||
+				Math.abs(cols[col]) == n ||
+				Math.abs(diag) == n ||
+				Math.abs(xdiag) == n) {
+
+				result = count > 0 ? 1 : 2;
+			}
+			System.out.println(result);
+			return result;
+		}
+	}	
+}
+
+
+
+
+
+
+
